16cfbf73abac3d8ff29d4c653564e0c93be6e5ab
Eugene <Bloschanenko@gmail.com>
2024-05-26 05:48:00 +0300

    hard work

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,22 @@
-const STORAGE_KEY = "options";
+const STORAGE_KEY = "feedback-form-state";
 
 const form  = document.querySelector(".feedback-form");
 const textarea = form.querySelector("textarea");
+const email = form.elements.email;
 
-function formSubmitHandler(e) {
+function formInputHandler(e) {
     e.preventDefault();
     const email = form.elements.email.value;
     const message = textarea.value;
     console.log(email, message);
-    const formData = JSON.stringify({email, message});
-    localStorage.setItem(STORAGE_KEY, formData);
-    form.reset();
+    const data = JSON.stringify({email, message});
+    localStorage.setItem(STORAGE_KEY, data);
+    // form.reset();
 };
 
-form.addEventListener('submit', formSubmitHandler);
+form.addEventListener('input', formInputHandler);
+const jsnData = localStorage.getItem(STORAGE_KEY) ?? " ";
+const formData = JSON.parse(jsnData);
+
+email.value = formData.email;
+textarea.value = formData.message;
