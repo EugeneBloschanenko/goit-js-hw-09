d267b285e38b15f2727a9791392a9f42bbe218db
Eugene <Bloschanenko@gmail.com>
2024-05-26 16:38:19 +0300

    finish

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,44 @@
+
+
 const STORAGE_KEY = "feedback-form-state";
 
-const form  = document.querySelector(".feedback-form");
+let formData = {
+    email: "",
+    message: "",
+};
+
+const form = document.querySelector(".feedback-form");
 const textarea = form.querySelector("textarea");
 const email = form.elements.email;
 
 function formInputHandler(e) {
-    e.preventDefault();
-    const email = form.elements.email.value;
-    const message = textarea.value;
-    console.log(email, message);
-    const data = JSON.stringify({email, message});
-    localStorage.setItem(STORAGE_KEY, data);
-    // form.reset();
-};
+    formData.email = email.value;
+    formData.message = textarea.value;
+    const data = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, data); 
+}
 
 form.addEventListener('input', formInputHandler);
-const jsnData = localStorage.getItem(STORAGE_KEY) ?? " ";
-const formData = JSON.parse(jsnData);
 
-email.value = formData.email;
-textarea.value = formData.message;
+try {
+    const jsnData = localStorage.getItem(STORAGE_KEY) ?? "";
+    formData = JSON.parse(jsnData);
+    email.value = formData.email;
+    textarea.value = formData.message;  
+} catch {
+    console.log("No saved data.");
+}
+
+function formSubmitHandler(e) {
+    e.preventDefault();
+    if (email.value || textarea.value) {
+        console.log(formData);
+        localStorage.removeItem(STORAGE_KEY);
+        form.reset();
+        formData = { email: "", message: "" };
+    } else {
+        alert("Fill please all fields"); 
+    }  
+}
+
+form.addEventListener('submit', formSubmitHandler);
